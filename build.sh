0ac02bfaaecb02f4925fd33a50cfe7b631c98351
Kyryl Ωliinyk <kyryloliinyk@gmail.com>
2025-07-30 20:30:44 +0300

    Add build package bash script

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+
+rm -rf build
+mkdir -p build
+
+pip install -r requirements.txt -t build/
+cp bot.py lambda_function.py build/
+
+cd build
+zip -r ../telegram_bot_deployment_package.zip .
+cd ..
+rm -rf build
