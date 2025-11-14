d9a3a2b2338b409010039d0ae2e04d3000d7c3e9
Kyryl Ωliinyk <kyryloliinyk@gmail.com>
2025-11-14 14:10:20 +0200

    Remove 'production' env code for lambda

--- config.py
+++ config.py
@@ -1,7 +1,4 @@
 from decouple import config
 
-
-WEBHOOK_URL = config("WEBHOOK_URL", default='')
 BOT_TOKEN = config("BOT_TOKEN")
 ADMIN_GROUP_ID = int(config("ADMIN_GROUP_ID"))
-ENV = config("ENV", default='development')
--- main.py
+++ main.py
@@ -1,28 +1,15 @@
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters
-from config import WEBHOOK_URL, BOT_TOKEN, ENV
+from config import BOT_TOKEN
 from handlers.user import start, handle_message
 from log_config import console_logger
 
 def main() -> None:
     console_logger.info("🤖 Happy Bot has been launched...")
 
-    if not WEBHOOK_URL and ENV == 'production':
-        console_logger.critical('WEBHOOK not found')
-        raise ValueError('WEBHOOK not found')
-
     app = ApplicationBuilder().token(BOT_TOKEN).build()
     app.add_handler(CommandHandler("start", start))
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
-
-    if ENV == 'production':
-        app.run_webhook(
-            listen="0.0.0.0",
-            port=8080,
-            webhook_url=WEBHOOK_URL,
-        )
-    else:
-        app.run_polling()
-
+    app.run_polling()
 
 if __name__ == "__main__":
     main()
