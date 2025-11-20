6a2ba95550dc806627764abe368251327de2d548
Kyryl Ωliinyk <kyryloliinyk@gmail.com>
2025-11-20 12:08:33 +0200

    Add file_logger & write logs to logs.log file

--- handlers/admin.py
+++ handlers/admin.py
@@ -2,7 +2,7 @@ from telegram import Message
 from telegram.ext import ContextTypes
 from state import user_states, message_to_user_map
 from keyboards import FEEDBACK_OPTS_KEYBOARD
-from log_config import console_logger
+from log_config import file_logger
 from messages import *
 
 async def handle_admin_reply(message: Message, context: ContextTypes.DEFAULT_TYPE, text: str) -> None:
@@ -13,13 +13,13 @@ async def handle_admin_reply(message: Message, context: ContextTypes.DEFAULT_TYP
     if recipient_id:
         match text.lower():
             case txt if txt == FEEDBACK_QUESTION.lower():
-                console_logger.info('Admin asked to rate support.')
+                file_logger.info('Admin asked to rate support.')
                 await context.bot.send_message(
                     chat_id=recipient_id,
                     text=FEEDBACK_REQUEST_MESSAGE,
                     reply_markup=FEEDBACK_OPTS_KEYBOARD
                 )
-                console_logger.info(f'User with id {recipient_id} was asked to rate support.')
+                file_logger.info(f'User with id {recipient_id} was asked to rate support.')
                 user_states[recipient_id] = "awaiting_feedback"
                 await message.reply_text(FEEDBACK_SENT_MESSAGE)
             case _:
@@ -27,8 +27,8 @@ async def handle_admin_reply(message: Message, context: ContextTypes.DEFAULT_TYP
                     chat_id=recipient_id,
                     text=f"{ADMIN_REPLY_LABEL}\n\n{text}"
                 )
-                console_logger.info(f'Admin unswer to user id {recipient_id} was sent.')
+                file_logger.info(f'Admin unswer to user id {recipient_id} was sent.')
                 await message.reply_text(ANSWER_SENT_MESSAGE)
     else:
-        console_logger.error('Unable to unswer. User not found.')
+        file_logger.error('Unable to unswer. User not found.')
         await message.reply_text(USER_NOT_FOUND_ANSWER_MESSAGE)
--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters
 from config import BOT_TOKEN
 from handlers.user import start, handle_message
-from log_config import console_logger
+from log_config import file_logger, clean_log_file, console_logger
 from error_handler import error_handler
 
 def main() -> None:
-    console_logger.info("🤖 Happy Bot has been launched...")
-
+    clean_log_file()
     app = ApplicationBuilder().token(BOT_TOKEN).build()
     app.add_error_handler(error_handler)
     app.add_handler(CommandHandler("start", start))
+    console_logger.info("🤖 Happy Bot has been launched...")
+    file_logger.info("🤖 Happy Bot has been launched...")
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
     app.run_polling()
 
